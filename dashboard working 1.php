4d8393b49a2739fac6384a872d0f6f5a5682fee3
thefuturekidsschool <62976571+thefuturekidsschool@users.noreply.github.com>
2020-05-08 08:24:34 +0530

    Add files via upload

---
+++ dashboard working 1.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html template="true"><head>
+
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>THE FUTURE KIDS SCHOOL, RJY</title></head><body style="color: rgb(245, 255, 167); background-color: rgb(0, 0, 66);" alink="#8ff7af" link="white" vlink="#ffcc33">
+<div style="text-align: center;"><big style="font-family: Arial; color: yellow;"><big><big><big><span style="font-family: Comic Sans MS;">The
+FUTURE KIDS SCHOOL, RAJAHMUNDRY</span></big></big></big></big><br style="color: rgb(255, 102, 0); font-family: Comic Sans MS;">
+<big style="font-family: Times New Roman,Times,serif; color: rgb(51, 204, 0);"><big><big><big>ONLINE
+MENTORING PROGRAM</big></big></big></big><br style="color: rgb(255, 102, 0);">
+<big style="font-family: Arial;"><big><big><span style="color: rgb(255, 204, 0);"></span></big></big></big><br>
+</div>
+
+<br>
+
+ <a href="%3C?php%20echo%20base_url%28%29;?%3Elogout">Logout</a>
+<table style="text-align: left; width: 958px; height: 386px; margin-left: auto; margin-right: auto;" border="0" cellpadding="15" cellspacing="4">
+
+  <tbody>
+    <tr>
+      <td style="vertical-align: top; width: 205px; color: rgb(204, 153, 51);"><big><big><br style="color: white;">
+      <big style="color: white;">Links</big><br>
+      </big></big></td>
+      <td style="vertical-align: top;"><br>
+      </td>
+      <td style="vertical-align: top;"><br>
+      </td>
+      <td colspan="3" rowspan="1" style="vertical-align: middle; text-align: center; width: 200px;"><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c1s"><big style="font-family: Arial;"><big><big><span style="color: rgb(51, 204, 0);">Online Lessons</span></big></big></big><br>
+      </a><big><big>  <a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c2s"><br>
+      </a></big></big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c3s"><big style="font-family: Arial;"><big><big><span style="color: rgb(255, 204, 0);">Please click on a class</span></big><span style="color: rgb(255, 204, 0);">
+      </span></big></big><br>
+      </a></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 205px; color: rgb(204, 153, 51);"><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/ZoomSchedule" target="_blank"><big><big><big>Zoom Class
+Schedule</big></big></big></a></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: middle; width: 200px; text-align: center;"><big><big><big><a rel="tag" href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c1s">Into Class I</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 202px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c2s">Into Class II</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 201px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c3s">Into Class III</a></big></big></big></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 205px; color: rgb(204, 153, 51);"><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/onlineclassinstructions.html" target="_blank"><big><big><big>Zoom
+Class Instructions</big></big></big></a></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: middle; width: 200px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c4s">Into Class IV</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 202px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c5s">Into Class V</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 201px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c6s">Into Class VI</a></big></big></big></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 205px; color: rgb(204, 153, 51);"><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/ompinstructions.html" target="_blank"><big><big><big>Instructions
+on Mentoring</big></big></big></a></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: middle; width: 200px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c7s">Into Class VII</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 202px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c8s">Into Class VIII</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 201px; text-align: center;"><big><big><big><a href="board/c9s">Into Class&nbsp; IX</a></big></big></big></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 205px; color: rgb(204, 153, 51);"><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/disclaimer" target="_blank"><big><big><big>Disclaimer</big>
+      </big></big></a></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: middle; width: 200px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c10s">Into Class X</a></big></big></big></td>
+      <td style="vertical-align: middle; width: 202px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c10ss">Class X (2019-20)</a></big></big></big></td>
+      <td style="width: 201px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c12s">Into Class XII</a></big></big></big></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 205px; color: rgb(204, 153, 51);"><big><br>
+      </big></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: top;"><big><br>
+      </big></td>
+      <td style="vertical-align: top; width: 200px; text-align: center;"><big><big><big><br>
+      </big></big></big></td>
+      <td style="vertical-align: top; width: 202px; text-align: center;"><big><big><big><a href="%3C?php%20echo%20base_url%28%29;?%3Eboard/c12ss">Class XII (2019-20)</a></big></big></big></td>
+      <td style="vertical-align: top; width: 201px; text-align: center;"><big><big><big><br>
+      </big></big></big></td>
+    </tr>
+  </tbody>
+</table>
+
+<br>
+
+<div style="text-align: center; background-color: rgb(0, 0, 66);"><big><big><br>
+</big>
+</big></div>
+
+</body></html>
